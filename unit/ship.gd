e45f71e6c86a9fa12ed183e5954e608195878f06
Xackery <xackery@gmail.com>
2024-02-06 10:44:50 -0800

    Add boss

--- unit/ship.gd
+++ unit/ship.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var speed = 3.0
-
+var shoot_timer = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,15 +9,18 @@ func _ready():
 
 
 func _physics_process(delta):
-	var is_right = false;
+	var is_right = false
 	if Input.is_action_pressed("ui_right"):
 		position.x += speed
-		is_right = true;
+		is_right = true
 	if !is_right and Input.is_action_pressed("ui_left"):
 		position.x -= speed
-	var is_down = false;
+	var is_down = false
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed
-		is_down = true;
+		is_down = true
 	if !is_down and Input.is_action_pressed("ui_up"):
 		position.y -= speed
+	if shoot_timer < Time.get_ticks_msec() and Input.is_action_pressed("ui_accept"):
+		shoot_timer = Time.get_ticks_msec()+1000
+		print("pew")
