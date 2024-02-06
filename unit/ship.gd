7c1adc19ce4c518578f3559a1ebad8fcf615497c
Xackery <xackery@gmail.com>
2024-02-06 10:26:07 -0800

    Add initial ship

---
+++ unit/ship.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var speed = 3.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	var is_right = false;
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed
+		is_right = true;
+	if !is_right and Input.is_action_pressed("ui_left"):
+		position.x -= speed
+	var is_down = false;
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed
+		is_down = true;
+	if !is_down and Input.is_action_pressed("ui_up"):
+		position.y -= speed
